3ad56c53f827c295e5e986d8c8dea2194b77462f
Felix <febuelvas1@misena.edu.co>
2023-11-18 11:51:07 -0500

    version 0.1 formulario sin codigo

---
+++ src/main/java/com/mycompany/ejercicio_formulario/Ejercicio_formulario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_formulario;
+
+/**
+ *
+ * @author felic
+ */
+public class Ejercicio_formulario {
+
+    public static void main(String[] args) {
+       
+    }
+}
